97d9faba7e587f63f2933c40df12d9eed0a95e3a
william da conceicao <william da conceicao>
2017-05-04 17:50:54 +0200

    Modification du 25-xorg.sh

---
+++ src/scripts/25-xorg.sh
@@ -0,0 +1,8 @@
+set -eux
+emerge x11-base/xorg-drivers x11-base/xorg-server
+sed -i 's:USE=":USE="X git networkmanager nls pulseaudio icu gnome-keyring opengl jpeg :' /etc/portage/make.conf
+echo 'INPUT_DEVICES="evdev"' >> /etc/portage/make.conf
+echo 'xterm' >> ~/.xinitrc
+env-update
+etc-update
+source /etc/profile
